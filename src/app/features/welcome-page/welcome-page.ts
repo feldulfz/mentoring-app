114db9aa020a7f02b4d7c4bcbf7033981c875032
felips_dulfz <felipinanerdulfo@gmail.com>
2025-12-04 11:02:53 +0800

    Fix Angular version mismatch

--- src/app/features/welcome-page/welcome-page.ts
+++ src/app/features/welcome-page/welcome-page.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
 import { Navbar } from "../../shared/components/navbar/navbar";
 import { Footer } from "../../shared/components/footer/footer";
 import { SlidingCard } from "../../shared/components/sliding-card/sliding-card";
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-welcome-page',
   imports: [RouterModule, Navbar, Footer, SlidingCard ],
